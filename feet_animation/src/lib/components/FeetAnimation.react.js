7b04faa7e188c3654d379c778b8e5b377884974d
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-01 19:45:06 +0100

    Display all sensor values

--- feet_animation/src/lib/components/FeetAnimation.react.js
+++ feet_animation/src/lib/components/FeetAnimation.react.js
@@ -8,8 +8,6 @@ import * as d3 from "d3";
  */
 const FeetAnimation = ({ id, setProps, width, height, sensorValues }) => {
 
-    console.log(sensorValues);
-
     // Format image (make stroke change on average sensor value)
     useEffect(() => {
         const svg = d3.select('#feet-image');
@@ -33,31 +31,43 @@ const FeetAnimation = ({ id, setProps, width, height, sensorValues }) => {
     // Display sensor values
     useEffect(() => {
         const svg = d3.select('#feet-image');
-
         const r = 7;
-        const x = 20;
-        const y = 35;
+        // TODO: Interpolate color value
         const color = 'purple';
-        const value = 1023;
 
-        const g = svg.append('g')
-            .classed('sensor-value', true);
 
-        g.append('circle')
-            .attr('cx', `${x}%`)
-            .attr('cy', `${y}%`)
-            .attr('r', `${r}%`)
-            .style('fill', color);
+        const positions = [
+            { x: 32, y: 33 },
+            { x: 10, y: 45 },
+            { x: 25, y: 90 },
 
-        const textY = y * 1.04;
-        const fontSize = r * 1;
-        g.append('text')
-            .attr('x', `${x}%`)
-            .attr('y', `${textY}%`)
-            .attr('text-anchor', 'middle')
-            .style('font-size', `${fontSize}px`)
-            .text(value);
+            { x: 68, y: 33 },
+            { x: 90, y: 45 },
+            { x: 74, y: 90 },
+        ];
 
+        for (const [i, position] of positions.entries()) {
+            const { x, y } = position;
+            const value = sensorValues[i];
+
+            const g = svg.append('g').classed('sensor-value', true);
+
+            g.append('circle')
+                .attr('cx', `${x}%`)
+                .attr('cy', `${y}%`)
+                .attr('r', `${r}%`)
+                .style('fill', color);
+
+            const transleteY = r * 0.29;
+            const fontSize = r * 1;
+            g.append('text')
+                .attr('x', `${x}%`)
+                .attr('y', `${y}%`)
+                .attr('transform', `translate(0 ${transleteY})`)
+                .attr('text-anchor', 'middle')
+                .style('font-size', `${fontSize}px`)
+                .text(value);
+        }
     });
 
     return (
