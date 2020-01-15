1d05b3b58dbde9b63689de6a8eb0c85f0f81871b
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-15 23:51:51 +0100

    Add dockstring to most of methods

--- index.py
+++ index.py
@@ -58,6 +58,8 @@ def map_value_to_RGB_string(value: float) -> str:
 
 
 def make_table(values, cell_colors=None):
+    """Create table for displaying sensor values from values and corresponding colors"""
+
     table = go.Figure(
         layout=dict(
             title=dict(
@@ -225,6 +227,7 @@ app.layout = html.Div(
     [State("collapse", "is_open")],
 )
 def toggle_collapse(n, is_open):
+    """Toggle more info about project"""
     if n:
         return not is_open
     return is_open
@@ -235,6 +238,8 @@ def toggle_collapse(n, is_open):
               [Input('last-anomaly', 'modified_timestamp')],
               [State('last-anomaly', 'data')])
 def update_last_anomaly(ts, data):
+    """Display data about most recent anomaly"""
+
     if ts is None:
         PreventUpdate
 
@@ -253,6 +258,8 @@ def update_last_anomaly(ts, data):
                ],
               [Input('tabs', 'value')])
 def on_person_tab_change(new_id):
+    """Switch current person"""
+
     key = f'personData{new_id}'
     preson = json.loads(store.lrange(key, 0, 0)[0])
 
@@ -271,6 +278,7 @@ def on_person_tab_change(new_id):
                Input('current-id', 'data')],
               [State('last-anomaly', 'data')])
 def update_anomaly_histogram(n_intervals, current_id, last_anomaly_data):
+    """Generate anomaly histogram and display it"""
 
     if current_id is None:
         raise PreventUpdate
@@ -307,6 +315,8 @@ def update_anomaly_histogram(n_intervals, current_id, last_anomaly_data):
               [Input('interval-component', 'n_intervals'),
                Input('current-id', 'data')])
 def update_feet_animation(_, current_id):
+    """Pass sensor data to Feet Animation custom component"""
+
     if current_id is None:
         raise PreventUpdate
 
@@ -323,6 +333,8 @@ def update_feet_animation(_, current_id):
               [Input('interval-component', 'n_intervals'),
                Input('current-id', 'data')],)
 def update_table(n_intervals, current_id):
+    """Create data for main table and draw it"""
+
     if current_id is None:
         raise PreventUpdate
 
@@ -360,6 +372,8 @@ def update_table(n_intervals, current_id):
                Input('single-sensor-tabs', 'value'),
                Input('current-id', 'data')])
 def update_singe_sensor_indicator(_, selected_sensor, current_id):
+    """Update analog style indicator"""
+
     if current_id is None or selected_sensor is None:
         raise PreventUpdate
 
